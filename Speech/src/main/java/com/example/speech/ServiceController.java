444a97019114e9ce73656f9abcef9a40fa2d00d5
haolinyang <903607767@qq.com>
2021-11-14 13:18:00 -0800

    update code

--- Speech/src/main/java/com/example/speech/ServiceController.java
+++ Speech/src/main/java/com/example/speech/ServiceController.java
@@ -6,11 +6,14 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.speech.v1.SpeechSettings;
 import org.checkerframework.checker.units.qual.A;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @RestController
@@ -32,6 +35,12 @@ public class ServiceController {
          * file should include string mp4 or wav
          */
         String filePath;
+        ClassPathResource classPathResource = new ClassPathResource("static/download/");
+
+        File f = new File(classPathResource.getPath());
+        if (!f.exists()) {
+            f.mkdirs();
+        }
 
         if (file.contains(".mp4")) {
             storageService.downloadAndConvertAndUpload(file);
--- Speech/src/main/java/com/example/speech/StorageService.java
+++ Speech/src/main/java/com/example/speech/StorageService.java
@@ -2,7 +2,9 @@ package com.example.speech;
 
 import com.google.api.gax.core.CredentialsProvider;
 import com.google.cloud.storage.*;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,8 +14,7 @@ import ws.schild.jave.encode.AudioAttributes;
 import ws.schild.jave.encode.EncodingAttributes;
 
 import javax.annotation.PostConstruct;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -73,7 +74,11 @@ public class StorageService {
         Storage storage = StorageOptions.newBuilder().setProjectId(projectId).build().getService();
         BlobId blobId = BlobId.of(bucketName, objectName);
         BlobInfo blobInfo = BlobInfo.newBuilder(blobId).build();
-        storage.create(blobInfo, Files.readAllBytes(Paths.get(filePath)));
+
+        ClassPathResource classPathResource = new ClassPathResource(filePath);
+        InputStream inputStream = classPathResource.getInputStream();
+
+        storage.create(blobInfo, inputStream.readAllBytes());
 
         System.out.println(
                 "File " + filePath + " uploaded to bucket " + bucketName + " as " + objectName);
@@ -81,13 +86,24 @@ public class StorageService {
 
     public void convertToWav (String fileFullName) {
         try {
-            String basePath = ResourceUtils.getURL("classpath:").getPath() + "static/download/";
-            String mp4FilePath = basePath + fileFullName;
-            String wavFileName = fileFullName.split("\\.")[0] + ".wav";
-            String wavFilePath = basePath + wavFileName;
+            ClassPathResource baseClassPathResource = new ClassPathResource("static/download/");
 
-            File source2 = new File(mp4FilePath);
+            ClassPathResource mp4ClassPathResource = new ClassPathResource("static/download/" + fileFullName);
+            InputStream mp4InputStream = mp4ClassPathResource.getInputStream();
+
+            ClassPathResource wavClassPathResource = new ClassPathResource("static/download/" + fileFullName.split("\\.")[0] + ".wav");
+
+            String baseMP4Path = mp4ClassPathResource.getPath();
+
+            String wavFileName = fileFullName.split("\\.")[0] + ".wav";
+            String wavFilePath = wavClassPathResource.getPath();
+
+            System.out.println(wavFilePath + ">>>>>>>");
+            convertInputStreamToFile(baseMP4Path, mp4InputStream);
+
+            File source2 = new File(baseMP4Path);
             File target2 = new File(wavFilePath);
+
             AudioAttributes audio2 = new AudioAttributes();
             audio2.setCodec("pcm_s16le");
             EncodingAttributes attrs2 = new EncodingAttributes();
@@ -109,19 +125,30 @@ public class StorageService {
             /**
              * this file name should include mp4 or wav
              */
-            String basePath = ResourceUtils.getURL("classpath:").getPath() + "static/download/";
-            String filePath = basePath + fileFullName;
-            File f = new File(basePath);
-            if (!f.exists()) {
-                f.mkdirs();
-            }
+            ClassPathResource classPathResource = new ClassPathResource("static/download/" + fileFullName);
+//            String basePath = ResourceUtils.getURL("classpath:").getPath() + "static/download/";
+            String filePath = classPathResource.getPath() + fileFullName;
+            InputStream inputStream = classPathResource.getInputStream();
+
+
             downloadObject("test-senti-frontend",
                     "test-senti-frontend.appspot.com", fileFullName,
-                    filePath, fileFullName);
+                    classPathResource.getPath(), fileFullName);
             return true;
         } catch (Exception e) {
             e.printStackTrace();
         }
         return false;
+    }
+
+    public void convertInputStreamToFile(String filePath, InputStream inputStream) {
+        File file = new File(filePath);
+        try(OutputStream outputStream = new FileOutputStream(file)){
+            IOUtils.copy(inputStream, outputStream);
+        } catch (FileNotFoundException e) {
+            // handle exception here
+        } catch (IOException e) {
+            // handle exception here
+        }
     }
 }
