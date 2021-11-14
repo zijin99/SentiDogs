0b737012c4486f13c62e4db5b56dc09a1de8aaa6
haolinyang <903607767@qq.com>
2021-11-14 01:52:55 -0800

    finish upload and convert

--- Speech/src/main/java/com/example/speech/ServiceController.java
+++ Speech/src/main/java/com/example/speech/ServiceController.java
@@ -4,6 +4,7 @@ import com.google.api.client.util.Lists;
 import com.google.api.gax.core.CredentialsProvider;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.speech.v1.SpeechSettings;
+import org.checkerframework.checker.units.qual.A;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,12 +21,25 @@ public class ServiceController {
     @Autowired
     private EntitySentimentService entitySentimentService;
 
-    @PostMapping(value = "file/{fileName}")
+    @Autowired
+    private StorageService storageService;
+
+    @RequestMapping(value = "file/{fileName}")
     @ResponseBody
     public List<SpeechEntity> getFileName(@PathVariable("fileName") String  file) {
 
-        String filePath = speechService.generatePath(file);
-//        System.out.println(filePath);
+        /**
+         * file should include string mp4 or wav
+         */
+        String filePath;
+
+        if (file.contains(".mp4")) {
+            storageService.downloadAndConvertAndUpload(file);
+            filePath = speechService.generatePath(file.split("\\.")[0] + ".wav");
+        } else {
+            filePath = speechService.generatePath(file);
+        }
+
         String trans = speechService.asyncRecognizeGcs(filePath);
         List<SpeechEntity> list = entitySentimentService.analyseSpeech(trans);
         List<SpeechEntity> sorted = entitySentimentService.generateList(list);
